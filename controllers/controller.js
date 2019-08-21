63eb4fb2a1ae344feddcc243e7d7b4769ad9c8f1
John Bell <johnrbell@gmail.com>
2019-08-21 09:20:56 -0400

    work on caching

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,9 @@
-var train_data = require('../parse.js')
+var trainData = require('../parse.js')
 
 exports.get_trains = (req,res) => {
   //generate cache time
   now = Date.parse(new Date)
-  console.log('cache is '+((now-json.cacheTime)/1000)+' seconds old')
+  // console.log('cache is '+((now-json.cacheTime)/1000)+' seconds old')
 
   //generate a token, serve index, fetch new train data from MTA
   token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
@@ -15,7 +15,7 @@ exports.post_trains = (req,res) => {
   //runs when we serve html and fetch new train data from MTA
   if (req.body.token == token){
     console.log('token match :)')
-    addCacheInfo(req.body.data)
+    trainData.addCacheInfo(req.body.data)
   }else{
     res.status(401)
   }
--- parse.js
+++ parse.js
@@ -1,3 +1,7 @@
-exports.addCache = trains => {
-	console.log(trains)
+exports.addCacheInfo = trains => {
+	// console.log(trains)
+	trainObj = {}
+	trainObj.trains = trains
+	trainObj.cacheTime = new Date
+	console.log(trainObj)
 }
